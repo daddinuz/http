c0baa8b1487faa7abf983b683e117b93f65ccf75
daddinuz <daddinuz@gmail.com>
2017-05-28 23:51:26 +0200

    stupid bench

--- bench.sh
+++ bench.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-echo "Python"
-time python3 -c "import requests; requests.get('https://api.github.com/repos/daddinuz/http/issues', headers={'Accept': 'application/vnd.github.v3+json', 'Content-Type': 'application/json', 'User-Agent': 'daddinuz/http'})"
+set -e
 
-echo
+x=1
+while [ ${x} -le 5 ]; do
+    echo "Python"
+    \time -p python3 -c "import requests; response = requests.get('https://api.github.com/repos/daddinuz/http/issues', headers={'Accept': 'application/vnd.github.v3+json', 'Content-Type': 'application/json', 'User-Agent': 'daddinuz/http'})" 2>&1
+    echo
 
-echo "C"
-time ./cmake-build-release/example
+    echo "C"
+    \time -p ./cmake-build-release/example 2>&1
+    echo
+
+    x=$(( ${x} + 1 ))
+done
